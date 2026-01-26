972ba3930478daa3b7fbd41f5c96b39638ec63f9
nather24 <nather24@gmail.com>
2026-01-26 23:16:01 +0300

    create folder structure

---
+++ assets/js/common.js
@@ -0,0 +1,13 @@
+/* Mobile menu toggle */
+function toggleMenu(){
+  document.getElementById("navLinks").classList.toggle("active");
+}
+/* Navbar scroll effect */
+window.addEventListener("scroll", function(){
+  var navbar = document.querySelector(".navbar");
+  if(window.scrollY > 30){
+    navbar.classList.add("scrolled");
+  }else{
+    navbar.classList.remove("scrolled");
+  }
+});
